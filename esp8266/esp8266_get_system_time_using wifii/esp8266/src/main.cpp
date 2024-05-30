bf7613039c9b1657c01032460e23039952700ce4
Trần Thanh Khoa <87396977+YuuKaa1239@users.noreply.github.com>
2024-05-31 03:25:19 +0700

    Add files via upload

---
+++ esp8266/esp8266_get_system_time_using wifii/esp8266/src/main.cpp
@@ -0,0 +1,41 @@
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);     // IF YOUR ADDRESS IS DIFFERENT, PUT THAT IN PLACE OF '0x3F'
+const char *ssid     = "P204 / 209";    // PUT YOUR WIFI NAME HERE
+const char *password = "30041975";      // PUT YOUR WIFI PASSWORD HERE
+WiFiUDP ntpUDP;
+#define offset 25200                    //UTC -1 = -3600,UTC +1 = 3600, UTC +2 = 7200 (FOR EVERY +1 HOUR ADD 3600,SUBTRACT 3600 FOR -1 HOUR)
+String weekDays[7]={"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"};
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+void setup() {
+  WiFi.begin(ssid, password);
+  timeClient.begin();
+  timeClient.setTimeOffset(offset);    
+  lcd.init();
+  lcd.backlight();
+  lcd.blink_on();
+  lcd.clear();
+}
+void loop() {
+  timeClient.update();
+  time_t epochTime = timeClient.getEpochTime();
+  String formattedTime = timeClient.getFormattedTime();
+  struct tm *ptm = gmtime ((time_t *)&epochTime);
+
+  int monthDay = ptm->tm_mday;
+  int currentMonth = ptm->tm_mon+1;
+  int currentYear = ptm->tm_year+1900;
+  String currentDate = String(monthDay)+"/"+String(currentMonth)+"/"+String(currentYear); 
+  String day=weekDays[timeClient.getDay()];
+  lcd.setCursor(0, 0);
+  lcd.print("DATE:"+currentDate);
+  lcd.setCursor(0, 1);
+  lcd.print("TIME:"+formattedTime);
+  lcd.setCursor(14, 1);
+  lcd.print(day);
+  delay(500);
+}
